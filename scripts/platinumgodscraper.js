db01c30d8fb273befd6ac08ac14c1516451ff58a
Surma <surma@surma.dev>
2023-09-03 12:11:12 +0100

    Scrapey scrape

---
+++ scripts/platinumgodscraper.js
@@ -0,0 +1,57 @@
+import { promises as fs } from "node:fs";
+import { chromium } from "playwright";
+
+const baseUrl = new URL("https://platinumgod.co.uk/all-items");
+const browser = await chromium.launch();
+const page = await browser.newPage();
+console.log("Opening platinumgod.co.uk...");
+await page.goto(baseUrl.toString(), { waitUntil: "domcontentloaded" });
+console.log("Scraping...");
+const parsedItems = await page.evaluate(async () => {
+	const items = [...document.querySelectorAll("li.textbox")];
+	const parsedItems = items.map((item) => {
+		const itemId = parseInt(item.dataset.sid, 10);
+		const itemName = item.querySelector(".item-title").textContent;
+		const description = [...item.querySelectorAll("p")]
+			.filter((p) => p.classList.length == 0)
+			.map((p) => p.textContent)
+			.join("\n");
+		const text = item.textContent;
+		const cs = window.getComputedStyle(item.querySelector(":scope .item"));
+		const image = cs.backgroundImage.slice(5, -2);
+		const offset = cs.backgroundPosition
+			.split(" ")
+			.map((v) => parseInt(v.slice(0, -2), 10));
+		return {
+			itemId,
+			itemName,
+			description,
+			icon: {
+				image,
+				offset,
+			},
+		};
+	});
+	return parsedItems;
+});
+await page.close();
+await browser.close();
+console.log("Saving items...");
+const itemsFile = new URL("../assets/items.json", import.meta.url).pathname;
+await fs.writeFile(itemsFile, JSON.stringify(parsedItems, null, "  "));
+console.log("Downloading sprite maps...");
+const spritemaps = await Promise.all(
+	[...new Set(parsedItems.map((i) => i.icon.image))].map(async (url) => {
+		const imageUrl = new URL(url, baseUrl);
+		const filename = imageUrl.pathname.split("/").at(-1);
+		return {
+			filename,
+			data: await fetch(imageUrl).then((r) => r.arrayBuffer()),
+		};
+	}),
+);
+console.log("Saving spritemaps...");
+for (const { filename, data } of spritemaps) {
+	const spriteFile = new URL("../assets/", import.meta.url).pathname + filename;
+	await fs.writeFile(spriteFile, new Uint8Array(data));
+}
